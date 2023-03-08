b760a709ef3bae9b2951a711df8e72f05d5b6b2e
1771245847 <55345096+1771245847@users.noreply.github.com>
2023-03-08 19:12:50 +0800

    2023-3-8

---
+++ footer.php
@@ -0,0 +1,53 @@
+<!--平台logo	-->
+	<div class="container-fluid logo-box">
+		<div class="container ">
+			<div class="row mTB20">
+				<div class="col-lg-12">
+					<blockquote>
+					本站均来于互联网，请勿用于任何商业用途，仅供学习交流
+					<footer> Copyright © 2023 All Rights Reserved </footer>
+					</blockquote>
+				</div>
+			</div>
+			<!--第一行-->
+			<div class="row">
+				<div class="col-xs-4 col-sm-2">
+					<a href="http://v.qq.com/" class="thumbnail" target="_blank" title="腾讯视频">
+						<img class="img-responsive" src="img/qq.png" />
+					</a>
+				</div>
+				<div class="col-xs-4 col-sm-2 ">
+
+						<a href="http://www.iqiyi.com/" class="thumbnail" target="_blank" title="爱奇艺">
+							<img class="img-responsive" src="img/iqiyi.png" />
+						</a>
+
+				</div>
+				<div class="col-xs-4 col-sm-2 ">
+
+						<a href="http://www.youku.com/" class="thumbnail" target="_blank" title="优酷">
+							<img class="img-responsive" src="img/youku.png" />
+						</a>
+				</div>
+			</div>
+			<!--第一行结束-->
+		</div>
+	</div>	
+	<footer class="footer">
+		<!--居中部分-->
+		</div>
+		<div class="container footer-tit">
+			<p><?php echo $aik['foot'];?></p>
+			<p>联系邮箱：<?php echo $aik['admin_email'];?></p>
+			<p class="toot-tto"><?php echo $aik['tongji'];?></p>
+			<!--统计展示-->			
+		</div>
+	</footer>
+    <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
+    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+    <!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件 -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/tv.js" /></script>
+	
+  </body>
+</html>
